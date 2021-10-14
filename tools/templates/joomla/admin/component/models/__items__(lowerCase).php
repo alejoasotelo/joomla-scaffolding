a28cd09c6dc86aa4f21d8fc0e411120b4261aba2
alejoasotelo <alesotelo@gmail.com>
2021-10-14 20:37:37 -0300

    [+] Agregue el guardado de la fecha de creación, modificación y el autor.

--- tools/templates/joomla/admin/component/models/__items__(lowerCase).php
+++ tools/templates/joomla/admin/component/models/__items__(lowerCase).php
@@ -30,11 +30,15 @@ class __component_name__Model__items__ extends JModelList
 	{
 		if (empty($config['filter_fields'])) {
 			$config['filter_fields'] = array(
-				'id',
-				'name',
+				'id', 'a.id',
+				'name', 'a.name',
 				'author',
-				'created',
-				'published'
+				'author_id',
+				'created', 'a.created',
+				'modified', 'a.modified',
+				'created_by', 'a.created_by',
+				'created_by_alias', 'a.created_by_alias',
+				'published', 'a.published',
 			);
 		}
 
@@ -57,8 +61,24 @@ class __component_name__Model__items__ extends JModelList
 			->from($db->quoteName('#____component_name__(lowerCase)___item__(lowerCase)', 'a'));
         
 		// Join with users table to get the username of the author
-		$query->select($db->quoteName('u.username', 'author'))
+		$query->select('u.username as author')
 			->join('LEFT', $db->quoteName('#__users', 'u') . ' ON u.id = a.created_by');
+
+
+		// Filter by author
+		$authorId = $this->getState('filter.author_id');
+
+		if (is_numeric($authorId))
+		{
+			$type = $this->getState('filter.author_id.include', true) ? '= ' : '<>';
+			$query->where('a.created_by ' . $type . (int) $authorId);
+		}
+		elseif (is_array($authorId))
+		{
+			$authorId = ArrayHelper::toInteger($authorId);
+			$authorId = implode(',', $authorId);
+			$query->where('a.created_by IN (' . $authorId . ')');
+		}
 
 		// Filter: like / search
 		$search = $this->getState('filter.search');
@@ -84,5 +104,71 @@ class __component_name__Model__items__ extends JModelList
 		$query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
 
 		return $query;
+	}
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
+	 */
+	protected function populateState($ordering = 'a.id', $direction = 'desc')
+	{
+		$app = JFactory::getApplication();
+
+		// Adjust the context to support modal layouts.
+		if ($layout = $app->input->get('layout'))
+		{
+			$this->context .= '.' . $layout;
+		}
+
+		$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
+
+		$published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
+		$this->setState('filter.published', $published);
+
+		$formSubmited = $app->input->post->get('form_submited');
+
+		$authorId   = $this->getUserStateFromRequest($this->context . '.filter.author_id', 'filter_author_id');
+
+		if ($formSubmited)
+		{
+
+			$authorId = $app->input->post->get('author_id');
+			$this->setState('filter.author_id', $authorId);
+		}
+
+		// List state information.
+		parent::populateState($ordering, $direction);
+	}
+
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param   string  $id  A prefix for the store id.
+	 *
+	 * @return  string  A store id.
+	 *
+	 * @since   1.6
+	 */
+	protected function getStoreId($id = '')
+	{
+		// Compile the store id.
+		$id .= ':' . $this->getState('filter.search');
+		$id .= ':' . $this->getState('filter.published');
+		$id .= ':' . serialize($this->getState('filter.author_id'));
+
+		return parent::getStoreId($id);
 	}
 }
--- tools/templates/joomla/admin/mvc/tables/__item__(lowerCase).php
+++ tools/templates/joomla/admin/mvc/tables/__item__(lowerCase).php
@@ -26,4 +26,56 @@ class __component_name__Table__item__ extends JTable
 	{
 		parent::__construct('#____component_name__(lowerCase)___item__(lowerCase)', 'id', $db);
 	}
+	/**
+	 * Stores a contact.
+	 *
+	 * @param   boolean  $updateNulls  True to update fields even if they are null.
+	 *
+	 * @return  boolean  True on success, false on failure.
+	 *
+	 * @since   1.6
+	 */
+	public function store($updateNulls = true)
+	{
+		$date   = JFactory::getDate()->toSql();
+		$userId = JFactory::getUser()->id;
+
+		// Set created date if not set.
+		if (!(int) $this->created)
+		{
+			$this->created = $date;
+		}
+
+		if ($this->id)
+		{
+			// Existing item
+			$this->modified_by = $userId;
+			$this->modified    = $date;
+		}
+		else
+		{
+			// Field created_by field can be set by the user, so we don't touch it if it's set.
+			if (empty($this->created_by))
+			{
+				$this->created_by = $userId;
+			}
+			
+			if (empty($this->created_by_alias))
+			{
+				$this->created_by_alias = JFactory::getUser()->username;
+			}
+
+			if (!(int) $this->modified)
+			{
+				$this->modified = $date;
+			}
+
+			if (empty($this->modified_by))
+			{
+				$this->modified_by = $userId;
+			}
+		}
+
+		return parent::store($updateNulls);
+	}
 }
