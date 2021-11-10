f63278a6b9225f59f7579790fc8ec0d04a8c1e52
alejoasotelo <alesotelo@gmail.com>
2021-11-10 19:16:19 -0300

    [+] Agregue soporte para los patrons __append y __replace en los archivos, para poder agregar contenido a archivos que ya existen o reemplazar en caso de que existan.

--- tools/generate.js
+++ tools/generate.js
@@ -1,4 +1,65 @@
 const {generateTemplateFiles} = require('generate-template-files');
+const fs = require('fs');
+
+let onComplete = async(results) => {
+
+    let REG_APPEND = '__append';
+    let REG_REPLACE_FULL = '__replace_full';
+    let REG_REPLACE = '__replace';
+    let REG_REPLACE_CONTENT = '###__replace__###';
+
+    let files = results.output.files.filter((file) => file.indexOf(REG_APPEND) >= 0 || file.indexOf(REG_REPLACE_FULL) >= 0);
+
+    for (let file of files) {
+
+        if (file.indexOf(REG_APPEND) >= 0) {
+            try {
+                if (fs.existsSync(file)) {
+                    let contentToAppend = fs.readFileSync(file);
+                    fileToAppend = file.replace(REG_APPEND, '');
+
+                    fs.appendFileSync(fileToAppend, contentToAppend);
+                    fs.unlinkSync(file);
+                }
+              } catch(err) {
+                console.error(err)
+              }
+        } else if (file.indexOf(REG_REPLACE_FULL) >= 0) {
+            // como es un reemplazo, tenemos 2 archivos 
+            // 1. El archivo solo la parte a agregar
+            // 2. El archivo completo en caso de que no exista
+
+            let fileComplete = file.replace(REG_REPLACE_FULL, '');
+            let fileReplace = file.replace(REG_REPLACE_FULL, REG_REPLACE);
+
+            // si ya existe en el destino el archivo completo, solo agrego.
+            if (fs.existsSync(fileComplete)) {
+                // elimino el archivo __replace_full porque no se usa
+
+                console.log('FILE COMPLETE', fileComplete);
+                console.log('FILE REPLACE', fileReplace);
+
+                let contentOfFileComplete = fs.readFileSync(fileComplete).toString('utf8');
+                let contentToReplace = fs.readFileSync(fileReplace).toString('utf8');
+
+                if (contentOfFileComplete.indexOf(REG_REPLACE_CONTENT) >= 0) {    
+                    let content = contentOfFileComplete.toString('utf8').replace(REG_REPLACE_CONTENT, contentToReplace.toString('utf8'));
+                    fs.writeFileSync(fileComplete, content);
+                } else {
+                    let itemName = results.stringReplacers.find((item) => item.slot.indexOf('__items__') >= 0 ? true : false).slotValue;
+                    fs.renameSync(fileReplace, fileReplace.replace(REG_REPLACE, itemName));
+                }
+
+                fs.unlinkSync(file);
+                fs.unlinkSync(fileReplace);
+
+            } else {
+                fs.renameSync(file, fileComplete);
+                fs.unlinkSync(fileReplace);
+            }
+        }
+    }
+}
 
 generateTemplateFiles([
     {
@@ -38,6 +99,7 @@ generateTemplateFiles([
             path: './dist/components/com___component_name__(lowerCase)/admin/',
             pathAndFileNameDefaultCase: '(lowerCase)',
         },
+        onComplete: onComplete
     },
     {
         option: 'Create Admin Controller',
@@ -62,17 +124,19 @@ generateTemplateFiles([
             path: './dist/components/com___component_name__(lowerCase)/admin/',
             pathAndFileNameDefaultCase: '(lowerCase)',
         },
+        onComplete: onComplete
     },
     {
         option: 'Create Admin View',
         defaultCase: '(pascalCase)',
         entry: {
-            folderPath: './tools/templates/joomla/admin/component/views/'
+            folderPath: './tools/templates/joomla/admin/views/'
         },
         stringReplacers: [{ question: 'Insert component name without "com_"', slot: '__component_name__' }, '__items__', '__item__'],
         output: {
-            path: './dist/components/com___component_name__(lowerCase)/admin/views/',
+            path: './dist/components/com___component_name__(lowerCase)/admin/',
             pathAndFileNameDefaultCase: '(lowerCase)',
         },
+        onComplete: onComplete
     }
 ]);
